8cc3b747e806b7bdd43733c8cd836ef2244963ef
oliwia <oliwia.giercza@gmail.com>
2023-05-02 13:17:57 +0200

    calculator test - learning get and activity scenario rule

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -1,5 +1,13 @@
 package com.example.tiptime
 
-class CalculatorTests {
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
+
+class CalculatorTests {
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
 }
